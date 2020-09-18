6bc1ffa4e397dcde6cfc8341097c8e460796fa4d
Ivan Zelenkov <ivan_zelenkov@dell.com>
2020-09-18 21:22:37 +0300

    cleanup

--- src/main/java/com/wine/to/up/demo/service/controller/KafkaController.java
+++ src/main/java/com/wine/to/up/demo/service/controller/KafkaController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
-import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
